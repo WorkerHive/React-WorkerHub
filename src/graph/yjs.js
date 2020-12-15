32ed3de2419142a21ce9a58098ea43df78f10f0e
Ross Leitch <professional.balbatross@gmail.com>
2020-12-15 19:26:16 +1300

    Redux CRUD, YJS Impl

--- src/graph/yjs.js
+++ src/graph/yjs.js
@@ -4,8 +4,7 @@ import { WebsocketProvider } from 'y-websocket';
 export default function (){
 
     const ydoc = new Y.Doc();
-    const host = "thetechcompany.workhub.services"
-    //const host = new URL(window.location).hostname;
+    const host = new URL(window.location).hostname;
     const websocketProvider = new WebsocketProvider(`wss://${host}:1234`, 'plan-editor', ydoc)
   
     const yArray = ydoc.getMap('')
