ff715823c93298e290e5090575051bc7eba605b9
Ross Leitch <professional.balbatross@gmail.com>
2020-12-23 18:23:48 +1300

    Mixing electron in to the cake

--- src/graph/index.js
+++ src/graph/index.js
@@ -1,18 +1,35 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { createUploadLink } from 'apollo-upload-client'
 
+import isElectron from 'is-electron';
+
 export default () => {
+
+  const getURL = () => {
+    let platform = 'web';
+
+    if(!process.env.NODE_ENV || process.env.NODE_ENV === 'development') platform = 'dev';
+    if(isElectron()) platform = 'electron';
+
+    switch(platform){
+      case 'web':
+        return '/graphql';
+      case 'dev':
+        return 'https://thetechcompany.workhub.services/graphql'
+      case 'electron':
+        return `https://${localStorage.getItem('workhub-api')}.workhub.services/graphql`
+    }
+  }
+
   try{
-  return new ApolloClient({
-    link: createUploadLink({
-      uri: (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? 
-        'https://thetechcompany.workhub.services/graphql' || 'http://localhost:4000/graphql' : 
-        process.env.REACT_APP_GRAPH_URL || '/graphql',
-    }),
-    cache: new InMemoryCache({
-      addTypename: false
+    return new ApolloClient({
+      link: createUploadLink({
+        uri: getURL()
+      }),
+      cache: new InMemoryCache({
+        addTypename: false
+      })
     })
-  })
   }catch(e){
     console.log(e)
   }
--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import {
     TextField,
     Button,
+    InputAdornment,
     Paper,
     Typography,
     Divider
 } from '@material-ui/core'
 
+import isElectron from 'is-electron';
 import { useMutation } from '@apollo/client';
 import { getQuote, getNodeConf, LOGIN, setToken } from '../../actions/authActions';
 import { connect } from 'react-redux';
@@ -17,6 +19,8 @@ import './index.css';
 function Login(props){
     const [ quote, setQuote ] = React.useState({})
     const [ error, setError ] = React.useState(null)
+
+    const [ workhub, setWorkhub ] = React.useState(localStorage.getItem('workhub-api'))
     const [ username, setUsername ] = React.useState('')
     const [ password, setPassword ] = React.useState('')
 
@@ -55,17 +59,34 @@ function Login(props){
             </div>
             <Paper style={{display: 'flex', flex: 0.3, flexDirection: 'column', padding: 8}}>
                 <div style={{display: 'flex', alignItems: 'center', paddingBottom: 8}}>
-                    <img style={{height: 70, marginRight: 12}} src="/assets/teal.png" />
+                    <img style={{height: 70, marginRight: 12}} src="./assets/teal.png" />
                     <Typography style={{color: 'teal'}} variant="h4">WorkHub</Typography>
 
                 </div>
                 <Divider style={{marginBottom: 22}} />
+
+            {isElectron() && (
+                <TextField  
+                    InputProps={{
+                        endAdornment: <InputAdornment position="end">.workhub.services</InputAdornment>
+                    }}
+                    marin="normal"
+                    value={workhub}
+                    onChange={e => {
+                        setWorkhub(e.target.value)
+                        localStorage.setItem('workhub-api', e.target.value)
+                    }}
+                    label="Workhub URL"
+                    />
+            )}
             <TextField 
+                margin="normal"
                 error={error}
                 label="Username" 
                 value={username} 
                 onChange={(e) => setUsername(e.target.value)}/>
             <TextField 
+                margin="normal"
                 error={error}
                 label="Password" 
                 type="password" 
